5abfdc235e14819c1fce54b5c4ff5970f267f411
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-06-03 16:00:46 -0700

    another

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - alphabet in lowercase, and then in uppercase, followed by a new line
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char a = 'a';
+  char b = 'A';
+while (a <= 'z')
+{
+putchar(a);
+a++;
+}
+while (b <= 'z')
+{
+putchar(b);
+b++;
+}
+putchar('\n');
+return (0);
+}
