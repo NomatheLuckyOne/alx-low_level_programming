aa2080b63703dffd1d70e5d2a3884f959eee1870
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-06-03 08:09:01 -0700

    another

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Progaram entry point
+ * main - Program entry point
  * Return: 0
  */
 int main(void)
 {
 
-puts("\"Programming is like building a multiptilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 
 }
