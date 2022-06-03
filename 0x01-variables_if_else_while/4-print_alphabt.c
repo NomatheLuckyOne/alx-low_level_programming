acef59bc039e881322b05ec4ed02979f89537711
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-06-03 16:37:48 -0700

    commiting

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main -  alphabet in lowercase, followed by a new line
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+if (i != 'e' && i != 'q')
+putchar(c);
+putchar('\n');
+return (0);
+}
