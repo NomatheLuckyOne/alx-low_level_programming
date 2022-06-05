7f75e65c499a5c3b5fe8afcb0464189a147c1ac8
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-06-05 09:20:44 -0700

    another

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 /**
- *main -  alphabet in lowercase, followed by a new line
+ *main - all single digit numbers of base 10 starting from 0, followed by a new line
  *
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
-char i;
-for (i = 'a'; i <= 'z'; i++)
+int i = 0
+for (i = 0; i < 0; i++)
 {
-if (i != 'e' && i != 'q')
-putchar(c);
+printf("%d\n", i);
 }
-putchar('\n');
 return (0);
 }
