088497108b5aca096f69bec7e5ca20647865f5d8
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-06-06 14:06:26 -0700

    another

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,35 @@
+nclude <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Write a program that prints all single numbers of base 10 from 0
+ *
+ *   * new line and a putchar used
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int n;
+
+
+
+			for (n = 48; n < 58; n++)
+
+					{
+
+						putchar(n);
+
+									}
+
+				putchar('\n');
+
+return (0);
+
+}
