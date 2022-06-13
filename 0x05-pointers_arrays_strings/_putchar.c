f8fe769401777ab7408d51f35080f34c4cc3f610
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-06-13 16:41:48 -0700

    commiting

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _putchar - alternates for original putchar () function from standard lib.
+ * @x: character parameter
+ *
+ * Return: void.
+ */
+
+void _putchar (char x)
+{
+	putchar (x);
+}
