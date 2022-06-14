71a00b261c7622bdd6c324b50cba2f76a1e18edf
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-06-13 17:04:34 -0700

    another

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main,h"
+
+/**
+ * _strien - calculate length of string
+ * @s: parameter
+ *
+ * Return: lengthof string.
+ */
+
+int _strlen(char *s)
+{
+	int myLen = strlen(s)
+
+	return (myLen);
+}
