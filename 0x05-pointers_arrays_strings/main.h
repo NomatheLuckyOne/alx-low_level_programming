feb61930212f7d7aa11ba86b656a028ddacd08cf
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-06-13 19:31:23 -0700

    anaother

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,15 +5,12 @@
 #include <stdio.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
-int _strlen(char *s);
+int _strlen(char *s)
 void _puts(char *str);
-void _putchar(char x);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-int a = strlen(str);
 int _putchar(char c)
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
