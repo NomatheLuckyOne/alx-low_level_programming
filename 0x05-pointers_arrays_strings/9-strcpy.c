d94d1b7ed9a4789af3375b85100dca2d05071081
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-06-14 00:56:16 -0700

    another

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,12 +5,12 @@
  * including the terminating null byte (\0),
  * to the buffer pointed to by dest.
  * @dest: parameter
- * @src: parameter 
+ * @src: parameter
  *
  * Return: dest, a char *.
  */
 
-char *_strcpy(char *dest, char *src
+char *_strcpy(char *dest, char *src)
 {
 	strcpy(dest, src);
 	return (dest);
