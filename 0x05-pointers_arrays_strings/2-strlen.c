3498ab1de4429d250023a42b60f1bce7675410da
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-06-13 17:13:33 -0700

    another

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,15 @@
-#include "main,h"
+#include "main.h"
 
 /**
- * _strien - calculate length of string
+ * _strlen - calculate length of string
  * @s: parameter
  *
- * Return: lengthof string.
+ * Return: length of string.
  */
 
 int _strlen(char *s)
 {
-	int myLen = strlen(s)
+	int myLen = strlen(s);
 
 	return (myLen);
 }
