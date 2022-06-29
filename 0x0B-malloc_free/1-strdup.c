7a0cc94dc2a0a5747adcce26040d5efe436474b5
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-06-28 18:58:49 -0700

    another

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,38 +2,29 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenate two strings using malloc
- * @s1: string 1
- * @s2: string 2
- * Return: pointer to concat string
+ * _strdup - Duplicate a string using malloc
+ * @str: string to duplicate
+ * Return: Pointer to a the new duped string
  */
 
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
 	char *a;
-	int i, j, c, d;
+	int i, c;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	if (str == NULL)
+		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
+	for (i = 0; str[i] != '\0'; i++)
 		;
 
-	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	a = malloc(i * sizeof(*a) + 1);
 	if (a == NULL)
 		return (NULL);
 
-	for (c = 0, d = 0; c < (i + j + 1); c++)
-	{
-		if (c < i)
-			a[c] = s1[c];
-		else
-			a[c] = s2[d++];
-	}
+	for (c = 0; c < i; c++)
+		a[c] = str[c];
+	a[c] = '\0';
 
 	return (a);
 }
